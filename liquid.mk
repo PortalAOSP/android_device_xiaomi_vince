e01c944a7ac3c31cb9c8796064e79dfba1cd0eaa
ManoloRey <cleitonmca@gmail.com>
2018-10-22 21:13:29 +0000

    Vince: Bringup LiquidRemix

--- liquid.mk
+++ liquid.mk
@@ -21,12 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from vince device
 $(call inherit-product, device/xiaomi/vince/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common LR stuff.
+$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vince
-PRODUCT_NAME := arrow_vince
+PRODUCT_NAME := liquid_vince
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5 Plus
 PRODUCT_MANUFACTURER := Xiaomi
