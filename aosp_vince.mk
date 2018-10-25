b4af603951f9d1810ac394b7abd7056870c92d10
4PERTURE <slicey2001@gmail.com>
2018-10-25 20:07:32 +0000

    PEX bringup

--- aosp_vince.mk
+++ aosp_vince.mk
@@ -21,12 +21,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from vince device
 $(call inherit-product, device/xiaomi/vince/device.mk)
 
-# Inherit some common LR stuff.
-$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
+# Inherit some common PEX stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES :=1080
+TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vince
-PRODUCT_NAME := liquid_vince
+PRODUCT_NAME := aosp_vince
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5 Plus
 PRODUCT_MANUFACTURER := Xiaomi
@@ -39,4 +41,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vince-user 8.1.0 OPM1.171019.019 V10.0.2.0.OEGMIFH release-keys"
 
 BUILD_FINGERPRINT := xiaomi/vince/vince:8.1.0/OPM1.171019.019/V10.0.2.0.OEGMIFH:user/release-keys
-
